960b609b022479982bd7a8d87eefc2211fd9e146
udodoff <udodovr@gmail.com>
2023-04-03 09:43:14 +0300

    comments

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+//init all dependencies
 const express = require('express')
-const app = express()
-const port = 3000
+const mysql = require('mysql2')
 
+//init express & port
+const app = express();
+const port = 3000;
+
+//endpoint for the "/"
 app.get('/', (req, res) => {
-    res.writeHead(200)
-    res.end('Hello World!')
-})
+    res.writeHead(200);         //sending status 200
+    res.end('Hello World!');    //sending responce "hello world"
+});
 
-
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => {        //listening port 3000
+    console.log(`Example app listening on port ${port}!`);
+});
